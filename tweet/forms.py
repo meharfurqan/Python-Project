513819bba1ed2bb12642bb0837a0af854ce4a8a6
meahrfurqan <meharfu8@gmail.com>
2025-10-23 16:40:15 +0500

    Moved project files to root directory

---
+++ tweet/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model= Tweet
+        fields= ['tweet','photo']
+
+
+
+# User Registration
+
+class UserRegistrationForm(UserCreationForm):
+    email=forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields= ('username', 'email','password1','password2')
